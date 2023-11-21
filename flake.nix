9663811618b8d86b395912b621ae09ecee1bdacc
WilliButz <willibutz@posteo.de>
2023-11-21 18:46:15 +0100

    update: 2023.10.3 -> 2023.10.4 (security update)
    Includes fix for https://goauthentik.io/docs/security/CVE-2023-48228
    
    Flake lock file updates:
    
    • Updated input 'authentik-src':
        'github:goauthentik/authentik/82b5274b15ddf6d9925e7b349f70bbff5be1d8be' (2023-11-09)
      → 'github:goauthentik/authentik/a2a67161ac8b840d63cbaacdfbebb60fd48e901b' (2023-11-21)

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       };
     };
     authentik-src = { # change version string in outputs as well when updating
-      url = "github:goauthentik/authentik/version/2023.10.3";
+      url = "github:goauthentik/authentik/version/2023.10.4";
       flake = false;
     };
   };
@@ -47,7 +47,7 @@
     { inherit inputs; }
     ({ inputs, lib, withSystem, ... }:
     let
-      authentik-version = "2023.10.3"; # to pass to the drvs of some components
+      authentik-version = "2023.10.4"; # to pass to the drvs of some components
     in {
       systems = [
         "x86_64-linux"
