d2367d0c21693f6c2d15b82feb0f2a5420559e42
WilliButz <willibutz@posteo.de>
2023-12-21 14:43:54 +0100

    update: 2023.10.4 -> 2023.10.5
    Flake lock file updates:
    
    • Updated input 'authentik-src':
        'github:goauthentik/authentik/a2a67161ac8b840d63cbaacdfbebb60fd48e901b' (2023-11-21)
      → 'github:goauthentik/authentik/a15a04036223c53bea841436f4943278b4eab460' (2023-12-21)

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       };
     };
     authentik-src = { # change version string in outputs as well when updating
-      url = "github:goauthentik/authentik/version/2023.10.4";
+      url = "github:goauthentik/authentik/version/2023.10.5";
       flake = false;
     };
   };
@@ -47,7 +47,7 @@
     { inherit inputs; }
     ({ inputs, lib, withSystem, ... }:
     let
-      authentik-version = "2023.10.4"; # to pass to the drvs of some components
+      authentik-version = "2023.10.5"; # to pass to the drvs of some components
     in {
       systems = [
         "x86_64-linux"
