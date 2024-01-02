8e23ad0cefb00523dbeb4d4d0b104e7aff1702e0
Alexander Sieg <alex@xanderio.de>
2024-01-02 11:45:20 +0100

    enable media uploads
    The media upload feature is build around being deployed in a container
    and only enables uploads when `/media` is a mountpoint. This isn't the
    case on nixos and as such media uploads are disable.
    
    In order to enable this, we need to patch authentik so that the
    `can_save_media` capability is enabled.

--- components/staticWorkdirDeps.nix
+++ components/staticWorkdirDeps.nix
@@ -1,10 +1,17 @@
 { authentik-src
 , authentikComponents
 , linkFarm
+, applyPatches
 }:
-
+let
+  patched-src = applyPatches { 
+    src = authentik-src;
+    name = "patched-authentik-source";
+    patches = [ ./authentik_media_upload.patch ];
+  };
+in
 linkFarm "authentik-static-workdir-deps" [
-  { name = "authentik"; path = "${authentik-src}/authentik"; }
+  { name = "authentik"; path = "${patched-src}/authentik"; }
   { name = "locale"; path = "${authentik-src}/locale"; }
   { name = "blueprints"; path = "${authentik-src}/blueprints"; }
   { name = "internal"; path = "${authentik-src}/internal"; }
