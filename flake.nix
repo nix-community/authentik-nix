367332e56eed4525cb676125c65c8a005c476785
Maximilian Bosch <maximilian@mbosch.me>
2025-09-16 10:02:18 +0200

    update: 2025.8.1 -> 2025.8.2
    ChangeLog: https://github.com/goauthentik/authentik/releases/tag/version%2F2025.8.2
    
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/af66ad14b28a127c5c0f3bbb298218fc63528a18' (2025-08-06)
      → 'github:hercules-ci/flake-parts/4524271976b625a4a605beefd893f270620fd751' (2025-09-01)
    • Updated input 'flake-parts/nixpkgs-lib':
        'github:nix-community/nixpkgs.lib/0f36c44e01a6129be94e3ade315a5883f0228a6e' (2025-07-27)
      → 'github:nix-community/nixpkgs.lib/a73b9c743612e4244d865a2fdee11865283c04e6' (2025-08-10)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/dfb2f12e899db4876308eba6d93455ab7da304cd' (2025-08-28)
      → 'github:NixOS/nixpkgs/c23193b943c6c689d70ee98ce3128239ed9e32d1' (2025-09-13)
    • Updated input 'pyproject-build-systems':
        'github:pyproject-nix/build-system-pkgs/6edb3ae27395cd88be3d64b732d1539957dad59c' (2025-08-25)
      → 'github:pyproject-nix/build-system-pkgs/5b8e37fe0077db5c1df3a5ee90a651345f085d38' (2025-09-08)
    • Updated input 'pyproject-nix':
        'github:pyproject-nix/pyproject.nix/030dffc235dcf240d918c651c78dc5f158067b51' (2025-08-28)
      → 'github:pyproject-nix/pyproject.nix/8d77f342d66ad1601cdb9d97e9388b69f64d4c8e' (2025-09-07)
    • Updated input 'uv2nix':
        'github:pyproject-nix/uv2nix/0529e6d8227517205afcd1b37eee3088db745730' (2025-08-29)
      → 'github:pyproject-nix/uv2nix/780494c40895bb7419a73d942bee326291e80b3b' (2025-09-15)

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     };
     authentik-src = {
       # change version string in outputs as well when updating
-      url = "github:goauthentik/authentik/version/2025.8.1";
+      url = "github:goauthentik/authentik/version/2025.8.2";
       flake = false;
     };
   };
@@ -67,7 +67,7 @@
         ...
       }:
       let
-        authentik-version = "2025.8.1"; # to pass to the drvs of some components
+        authentik-version = "2025.8.2"; # to pass to the drvs of some components
       in
       {
         systems = import inputs.systems;
